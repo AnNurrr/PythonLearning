fd300d8c2377d8ffcff8b03ad6cbe388a0e53d71
AnNurrr <ainuremilbekova9@gmail.com>
2023-12-02 17:48:12 -1100

    changes

--- Module1/turtle_object.py
+++ Module1/turtle_object.py
@@ -1,7 +1,8 @@
 import turtle
 screen = turtle.Screen()
 t = turtle.Turtle()
-
+t.fillcolor("red")
+t.begin_fill()
 t.left(40)
 t.forward(50)
 t.right(40)
@@ -14,4 +15,15 @@ t.right(50)
 t.forward(170)
 t.right(90)
 t.forward(160)
+t.right(40)
+t.forward(70)
+t.right(60)
+t.forward(60)
+t.right(30)
+t.forward(30)
+t.right(50)
+t.forward(50)
+t.end_fill()
+
 screen.exitonclick()
+
