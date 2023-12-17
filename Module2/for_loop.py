6d7f9bfa8de6be9337633909ff1892bfcdce62a1
AnNurrr <ainuremilbekova9@gmail.com>
2023-12-17 12:30:11 -0600

    exercises

---
+++ Module2/for_loop.py
@@ -0,0 +1,10 @@
+#12/17/2023
+
+# for variable in iterable:
+# Code to be executed for each iteration
+
+numbers = [1, 2, 3, 4, 5, 6, 7]
+
+for num in numbers:
+    squared = num**2
+    print(f"The square of {num} is {squared}")
---
+++ Module2/if_else.py
@@ -0,0 +1,11 @@
+# Ainur
+# 12/17/2023
+
+
+message = int(input("Please enter a number: "))
+
+
+if message%2==0:
+    print(message, "is even number")
+else:
+    print(message, "is odd number")
